b4d311490f0cfcf772720ac84836df90a42576d1
Romina2033 <rogonzalez2033@gmail.com>
2024-11-12 16:53:37 -0300

    cambio de js a html anuncio

--- desafios/js/desafios.js
+++ desafios/js/desafios.js
@@ -1,20 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Crear el popup
-    const anuncio = document.createElement('div');
-    anuncio.id = 'anuncio';
-    anuncio.className = 'alert alert-warning text-center p-4 mt-4';
-    
-    anuncio.innerHTML = `
-        <button class="close-btn">&times;</button>
-        <div class="container">
-            <h3 class="alert-heading">¡Atención!</h3>
-            <p class="mb-4">¿Quieres completar los desafíos al 100%? Consulta nuestra guía completa para obtener todos los consejos y trucos necesarios.</p>
-            <a href="ayuda/ayuda.html" class="btn btn-dark btn-lg">¡Haz clic aquí!</a>
-        </div>
-    `;
+    const anuncio = document.getElementById('anuncio');
 
-    // Insertar el anuncio en el documento
-    document.body.appendChild(anuncio);
 
     // Función para mostrar el popup
     function mostrarPopup() {
