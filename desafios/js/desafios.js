bfc0b74f9601b421dcf1196b2b418976abe567da
Romina2033 <rogonzalez2033@gmail.com>
2024-11-17 15:05:04 -0300

    cambio de en vez de pop out a um simple boton (aun falta corregirlo) uwu

--- desafios/js/desafios.js
+++ desafios/js/desafios.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Crear el popup
-    const anuncio = document.getElementById('anuncio');
-
-
-    // Función para mostrar el popup
-    function mostrarPopup() {
-        anuncio.classList.add('mostrar');
-    }
-
-    // Función para ocultar el popup y hacer que reaparezca en 10 segundos
-    function ocultarPopup() {
-        anuncio.classList.remove('mostrar');
-        setTimeout(mostrarPopup, 10000); // Reaparece después de 10 segundos
-    }
-
-    // Mostrar el popup tras una pequeña demora inicial
-   /* setTimeout(mostrarPopup);*/
-
-    // Cerrar el popup al hacer clic en la "X"
-    const closeBtn = anuncio.querySelector('.close-btn');
-    closeBtn.addEventListener('click', ocultarPopup);
-});
-
 
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
