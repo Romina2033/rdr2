4f9d79bac72333dce075393ae765f1d32ade2f50
Romina2033 <rogonzalez2033@gmail.com>
2024-11-25 20:47:18 -0300

    cambio a columnas, agregado de acordeon, cambio de tamaño strong en desafio y ponerlo mas bonito uwu

--- desafios/js/desafios.js
+++ desafios/js/desafios.js
@@ -10,3 +10,19 @@ menuIcon.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     document.body.classList.remove('menu-open');
 });
+
+document.querySelectorAll('.ver-mas').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const parent = btn.closest('.desafio-box'); 
+        const hiddenContent = parent.querySelector('.hidden'); 
+
+
+        if (hiddenContent.style.maxHeight) {
+            hiddenContent.style.maxHeight = null; 
+            btn.textContent = 'Ver más'; 
+        } else {
+            hiddenContent.style.maxHeight = hiddenContent.scrollHeight + 'px'; 
+            btn.textContent = 'Ver menos'; 
+        }
+    });
+});
